c2be952fd5105bcf321fcdac0d53d4ce8fc8ed81
Lukasz Komsta <lukasz.komsta@umlub.pl>
2015-01-05 00:00:00 +0000

    version 0.14

--- R/agostino.test.R
+++ R/agostino.test.R
@@ -12,9 +12,9 @@ s3 <- (sum((x-mean(x))^3)/n)/(sum((x-mean(x))^2)/n)^(3/2)
 y <- s3*sqrt((n+1)*(n+3)/(6*(n-2)))
 b2 <- 3*(n*n+27*n-70)*(n+1)*(n+3)/((n-2)*(n+5)*(n+7)*(n+9))
 w <- sqrt(-1+sqrt(2*(b2-1)));
-d <- 1/sqrt(log10(w));
+d <- 1/sqrt(log(w));
 a <- sqrt(2/(w*w-1));
-z <- d*log10(y/a+sqrt((y/a)^2+1));
+z <- d*log(y/a+sqrt((y/a)^2+1));
      pval <- pnorm(z, lower.tail = FALSE)
 if (alter == 0) {
 pval <- 2*pval
